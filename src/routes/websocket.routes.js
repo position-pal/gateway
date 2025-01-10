b3f4d78033d5f4b9ce2e3e95fc8d52e8ce5e6506
Valerio Di Zio <diziovalerio@gmail.com>
2025-01-10 10:54:41 +0100

    feat(ws): implement chat service ws

--- src/routes/websocket.routes.js
+++ src/routes/websocket.routes.js
@@ -5,11 +5,14 @@ require('dotenv').config();
 
 const LOCATION_SERVICE_URL = process.env.LOCATION_SERVICE_URL || "localhost:8080";
 const LOCATION_SERVICE_API_VERSION = process.env.LOCATION_SERVICE_API_VERSION || "v1";
+
+const CHAT_SERVICE_URL = process.env.CHAT_SERVICE_URL || "localhost:8081";
+const CHAT_SERVICE_API_VERSION = process.env.CHAT_SERVICE_API_VERSION || "v1";
 const router = express.Router();
 expressWs(router);
 
 /*
-EXAMPLE MESSAGE
+EXAMPLE MESSAGE FOR LOCATION SERVICE
 const message = {
   "SampledLocation":{
     "timestamp":"2024-12-22T10:40:18.967675Z",
@@ -22,6 +25,7 @@ const message = {
   }
 }
 */
+
 
 router.ws('/location/:group/:user', (ws, req) => {
   const { group, user } = req.params;
@@ -44,6 +48,31 @@ router.ws('/location/:group/:user', (ws, req) => {
 
   ws.on('close', () => {
     location_ws.close();
+    console.log('WebSocket connection closed');
+  });
+});
+
+router.ws('/chat/:group/:user', (ws, req) => {
+  const { group, user } = req.params;
+
+  const chat_ws = new WebSocket(`ws://${CHAT_SERVICE_URL}/${CHAT_SERVICE_API_VERSION}/messages/${group}?user=${user}`);
+
+  // Forward messages from client to chat_ws
+  ws.on('message', (msg) => {
+    chat_ws.send(msg);
+  });
+
+  // Forward messages from chat_ws to client
+  chat_ws.on('message', (msg) => {
+    ws.send(msg);
+  });
+
+  ws.on('error', (error) => {
+    console.error('WebSocket error:', error);
+  });
+
+  ws.on('close', () => {
+    chat_ws.close();
     console.log('WebSocket connection closed');
   });
 });
