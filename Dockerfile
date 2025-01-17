4eea5dd5d6a4ff37f9a3d526ea69c5de1bae54f1
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-01-17 17:39:01 +0100

    build: configure release, dockerfile and it’s publication  (#17)
    * build: create dockerfile
    
    * ci: add dry-delivery and docker image publish steps
    
    * chore(gitignore): track also package-lock.json
    
    * build: use node version 22
    
    * build: update to last dependencies
    
    * fix(build): move grpc-js as production dependency
    
    * build(dockerfile): do not copy recursevely since can inadvertently add sensitive data to container
    
    * fix(dockerfile): do not use root user
    
    * fix(dockerfile): improve security by running npm ci with non-root user
    
    * fix(dockerfile): improve security by running npm ci with non-root user
    
    * fix(dockerfile): move to node 22
    
    * fix(dockerfile): use multistages
    
    * fix: root issues

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:22 AS build
+WORKDIR /app
+COPY package.json package-lock.json ./
+RUN npm ci --omit=dev --ignore-scripts
+COPY src ./src
+
+FROM node:22
+RUN groupadd -r nonroot && useradd -r -g nonroot nonroot
+WORKDIR /app
+# Create and set npm cache directory with correct permissions
+ENV NPM_CONFIG_CACHE=/app/.npm
+RUN mkdir -p /app/.npm && chown -R nonroot:nonroot /app
+USER nonroot
+COPY --from=build --chmod=755 /app .
+EXPOSE 3000
+
+CMD ["node", "src/server.js"]
---
+++ release.config.js
@@ -0,0 +1,19 @@
+const publishCommands = `
+echo "RELEASE_STATUS=released" >> $GITHUB_ENV
+echo "RELEASE_VERSION="\${nextRelease.version} >> $GITHUB_ENV
+`;
+const releaseBranches = ["main"];
+const config = require("semantic-release-preconfigured-conventional-commits");
+config.branches = releaseBranches;
+config.plugins.push(
+  // Custom release commands
+  [
+    "@semantic-release/exec",
+    {
+      publishCmd: publishCommands,
+    },
+  ],
+  "@semantic-release/github",
+  "@semantic-release/git",
+);
+module.exports = config;
