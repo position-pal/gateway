4f302bd7711e189b01b3331067a7620304e037b9
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-01-24 22:32:58 +0100

    test: add test env hooks configuration

---
+++ tests/features/step_definitions/common.js
@@ -0,0 +1,22 @@
+const { BeforeAll, AfterAll } = require("@cucumber/cucumber");
+const { execSync } = require("child_process");
+
+BeforeAll(async () => {
+  console.log("Bring up the local testing environment");
+  try {
+    execSync(`/bin/bash ./local-deployment/local-deploy.sh up`, { stdio: "inherit" });
+  } catch (error) {
+    console.error("Error while setting up the test environment:", error);
+    process.exit(1);
+  }
+})
+
+AfterAll(async () => {
+  console.log("Tearing down the local testing environment");
+  try {
+    execSync("/bin/bash ./local-deployment/local-deploy.sh down", { stdio: "inherit" });
+  } catch (error) {
+    console.error("Error while tearing down the test environment:", error);
+    process.exit(1);
+  }
+})
