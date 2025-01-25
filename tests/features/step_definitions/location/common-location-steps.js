3e63ef8d2386ec89378f14fe779c859c82efd718
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-01-25 08:38:02 +0100

    test: setup test users

--- tests/features/step_definitions/location/common-location-steps.js
+++ tests/features/step_definitions/location/common-location-steps.js
@@ -1,5 +1,50 @@
 const { Given } = require("@cucumber/cucumber");
+const { expect } = require("chai");
+const axios = require("axios");
 
-Given("I'm a logged user", () => {});
+const gatewayEndpoint = "http://127.0.0.1:3000";
+
+Given("I'm a logged user", async () => {
+  if (!global.luke) {
+    console.log("Logging in as Luke");
+    global.luke = await setupUser({
+      userData: {
+        name: "Luke",
+        surname: "Skywalker",
+        email: "skywalker@gmail.com",
+        role: "user",
+      },
+      password: "I'm sexy and I know it",
+      group: "astro",
+    });
+    console.log("Logging in as Leia");
+    global.leia = await setupUser({
+      userData: {
+        name: "Leia",
+        surname: "Organa",
+        email: "leila.organa@gmail.com",
+        role: "user",
+      },
+      password: "Please_Help_Me_Obi-Wan",
+      group: "astro",
+    });
+  }
+  console.log("Already logged in as Luke");
+});
 
 Given("I'm in a group with other users", () => {});
+
+const pick = (obj, keys) => Object.fromEntries(keys.map(key => [key, obj[key]]));
+
+async function setupUser(userDetails) {
+  const registrationData = pick(userDetails, ["userData", "password"]);
+  const registrationResponse = await axios.post(`${gatewayEndpoint}/api/users`, registrationData);
+  expect(registrationResponse.status).to.eq(201);
+  const loginData = { username: userDetails.userData.email, password: userDetails.password };
+  const authResponse = await axios.post(`${gatewayEndpoint}/api/auth/login`, loginData);
+  expect(authResponse.status).to.eq(200);
+  return {
+    ...userDetails,
+    token: authResponse.data.token
+  };
+}
--- tests/features/utils/users-groups-utils.js
+++ tests/features/utils/users-groups-utils.js
@@ -1,9 +1,28 @@
-const astroGroupId = "astro";
-const luke = "luke";
-const leia = "leia";
+const luke = {
+  userData: {
+    name: "Luke",
+    surname: "Skywalker",
+    email: "skywalker@gmail.com",
+    role: "user",
+  },
+  password: "I'm sexy and I know it",
+  group: "astro",
+  token: null,
+};
+
+const leia = {
+  userData: {
+    name: "Leia",
+    surname: "Organa",
+    email: "leila.organa@gmail.com",
+    role: "user",
+  },
+  password: "Please_Help_Me_Obi-Wan",
+  group: "astro",
+  token: null,
+}
 
 module.exports = {
-  astroGroupId,
   luke,
   leia,
 };
