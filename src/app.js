2871705b763b21f2a8ea4903578e873c09fc0809
Giovanni Antonioni <antonioni.giovanni9@gmail.com>
2025-02-17 14:05:43 +0100

    fix: cors adding options to allowed methods (#80)

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ expressWs(app);
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
