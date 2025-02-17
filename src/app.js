3cb26881f71f17ff80338f0b0144773c8dbbc2f3
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-02-17 13:01:28 +0100

    fix: add headers for cors validation

--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ const app = express();
 expressWs(app);
 
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 
 app.use("/api", jwtAuth);
 app.use("/api", routes);
