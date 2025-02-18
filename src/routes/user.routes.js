f6dbac0801bc5999989bd484648f7cdd72e51bff
Valerio Di Zio <diziovalerio@gmail.com>
2025-02-18 11:33:18 +0100

    feat: getUserByEmail

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,7 @@ const userController = require("../controllers/user.controller");
 const router = express.Router();
 
 router.get("/:id", userController.getUser);
+router.get("/:email", userController.getUserByEmail);
 router.post("/", userController.createUser);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
